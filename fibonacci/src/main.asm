6a448e50aac0d5d5bcee7274584e1594b8fe4a93
mon1t0r <westxlebyshek@gmail.com>
2025-05-12 17:39:51 +0200

    Fibonacci improvements

--- fibonacci/src/main.asm
+++ fibonacci/src/main.asm
@@ -23,9 +23,39 @@ _start:
 	test eax, eax        ; if numbers count is 0, jump to .end
 	jz .end
 
-	mov ecx, eax         ; numbers count
-	mov eax, 1           ; cur val
+; First two numbers
+
+	mov ebx, eax         ; move EAX to EBX so it is not erased by prints
+
+	push dword 0         ; print 0
+	call print_u
+	add esp, 4           ; cleanup after call
+
+	dec ebx              ; decrement numbers count
+	test ebx, ebx        ; if is zero, jump to .end_print
+	jz .end_print
+
+	push delim           ; print delimiter
+	call print_s
+	add esp, 4           ; cleanup after call
+
+	push dword 1         ; print 1
+	call print_u
+	add esp, 4           ; cleanup after call
+
+	dec ebx              ; decrement numbers count
+	test ebx, ebx        ; if is zero, jump to .end_print
+	jz .end_print
+
+	push delim           ; print delimiter
+	call print_s
+	add esp, 4           ; cleanup after call
+
+; Main loop
+
+	mov ecx, ebx         ; numbers count
 	xor ebx, ebx         ; prev val
+	mov eax, 1           ; cur val
 
 .loop:
 	mov edx, eax         ; move cur val to temp
@@ -35,14 +65,14 @@ _start:
 	push ecx             ; save ECX
 	push eax             ; save EAX
 
-	push eax             ; set print number from EAX
+	push eax             ; print EAX
 	call print_u
 	add esp, 4           ; cleanup after call
 
 	cmp dword [esp+4], 1 ; do not print delim at last iteration ([esp+4]=ECX)
 	je .after_delim
 
-	push delim           ; set print str to delim
+	push delim           ; print delimiter
 	call print_s
 	add esp, 4           ; cleanup after call
 .after_delim:
@@ -51,6 +81,9 @@ _start:
 	pop ecx              ; restore ECX
 	loop .loop
 
+; End section
+
+.end_print:
 	push 10              ; \n character
 	call print_c
 	add esp, 1           ; cleanup after call
