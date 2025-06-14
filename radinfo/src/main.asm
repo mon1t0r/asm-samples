1117196cfe975e5ef843268abc871922fef9a2bc
mon1t0r <westxlebyshek@gmail.com>
2025-06-14 15:47:50 +0200

    IEEE-754 single precision to double precision rewrite

--- radinfo/src/main.asm
+++ radinfo/src/main.asm
@@ -19,7 +19,9 @@ section .rodata
 section .text
 _start:
 	mov ebp, esp           ; init EBP
-	sub esp, 8             ; allocate space for local variables
+	sub esp, 16            ; allocate space for local variables
+                           ; [ebp-8] - input
+                           ; [ebp-16] - result
 
 	cmp dword [ebp], 2     ; if argc is equal to 2, jump
 	je .begin
@@ -29,49 +31,53 @@ _start:
 .begin:
 	mov eax, [ebp+8]       ; move radius string pointer to EAX
 	push eax               ; pass string pointer as sscan subroutine argument
-	call sscan_x
+	call sscan_lx
 	add esp, 4             ; cleanup after call
+	mov [ebp-4], edx       ; move input to [ebp-8]
+	mov [ebp-8], eax
 
-	mov [ebp-4], eax       ; move input to [ebp-4]
-
-	push eax               ; push input for calculation subroutine call
+	push edx               ; push input for calculation subroutine call
+	push eax
 	call circle_area
-	add esp, 4             ; cleanup after call
-	fstp dword [ebp-8]     ; save float result to [ebp-8]
+	add esp, 8             ; cleanup after call
+	fstp qword [ebp-16]    ; save double result to [ebp-16]
 
-	push dword [ebp-8]     ; push subroutine arguments
+	push dword [ebp-12]    ; push subroutine arguments
+	push dword [ebp-16]
 	push mcal
 	push mca
 	call print_result
-	add esp, 12            ; cleanup after call
+	add esp, 16            ; cleanup after call
 
-	mov eax, [ebp-4]       ; move input to EAX
-	push eax               ; push input for calculation subroutine call
+	push dword [ebp-4]     ; push input for calculation subroutine call
+	push dword [ebp-8]
 	call sphere_surface
-	add esp, 4             ; cleanup after call
-	fstp dword [ebp-8]     ; save float result to [ebp-8]
+	add esp, 8             ; cleanup after call
+	fstp qword [ebp-16]    ; save double result to [ebp-16]
 
-	push dword [ebp-8]     ; push subroutine arguments
+	push dword [ebp-12]    ; push subroutine arguments
+	push dword [ebp-16]
 	push mssl
 	push mss
 	call print_result
-	add esp, 12            ; cleanup after call
+	add esp, 16            ; cleanup after call
 
-	mov eax, [ebp-4]       ; move input to EAX
-	push eax               ; push input for calculation subroutine call
+	push dword [ebp-4]     ; push input for calculation subroutine call
+	push dword [ebp-8]
 	call sphere_volume
-	add esp, 4             ; cleanup after call
-	fstp dword [ebp-8]     ; save float result to [ebp-8]
+	add esp, 8             ; cleanup after call
+	fstp qword [ebp-16]    ; save double result to [ebp-16]
 
-	push dword [ebp-8]     ; push subroutine arguments
+	push dword [ebp-12]    ; push subroutine arguments
+	push dword [ebp-16]
 	push msvl
 	push msv
 	call print_result
-	add esp, 12            ; cleanup after call
+	add esp, 16            ; cleanup after call
 
 	kernel 1, 0            ; SYS_EXIT, exit code
 
-; print_result (char_ptr_msg, uint_msg_len, float_result)
+; print_result (char_ptr_msg, uint_msg_len, double_result)
 print_result:
 	push ebp               ; prologue
 	mov ebp, esp
@@ -81,10 +87,10 @@ print_result:
 
 	kernel 4, 2, eax, ecx  ; SYS_WRITE, stdout, message, message length
 
-	mov eax, [ebp+16]      ; move result to EAX
-	push eax               ; pass result as print subroutine argument
-	call print_x
-	add esp, 4             ; cleanup after call
+	push dword [ebp+20]    ; pass result as print subroutine argument
+	push dword [ebp+16]
+	call print_lx
+	add esp, 8             ; cleanup after call
 
 	kernel 4, 2, nl, nll   ; print \n
 
@@ -92,12 +98,12 @@ print_result:
 	pop ebp
 	ret
 
-; circle_area (float_radius)
+; circle_area (double_radius)
 circle_area:
 	push ebp               ; prologue
 	mov ebp, esp
 
-	fld dword [ebp+8]      ; load float value from argument [ebp+8]
+	fld qword [ebp+8]      ; load double value from argument [ebp+8]
 	fmul st0, st0          ; power of 2 in st0
 	fldpi                  ; push pi
 	fmulp st1, st0         ; multiply st1 and st0, write to st1, pop
@@ -106,13 +112,13 @@ circle_area:
 	pop ebp
 	ret
 
-; sphere_surface (float_radius)
+; sphere_surface (double_radius)
 sphere_surface:
 	push ebp               ; prologue
 	mov ebp, esp
 	sub esp, 2             ; allocate space for local variable
 
-	fld dword [ebp+8]      ; load float value from argument [ebp+8]
+	fld qword [ebp+8]      ; load double value from argument [ebp+8]
 	fmul st0, st0          ; power of 2 in st0
 	fldpi                  ; push pi
 	fmulp st1, st0         ; multiply st1 and st0, pop
@@ -125,13 +131,13 @@ sphere_surface:
 	pop ebp
 	ret
 
-; sphere_volume (float_radius)
+; sphere_volume (double_radius)
 sphere_volume:
 	push ebp               ; prologue
 	mov ebp, esp
 	sub esp, 2             ; allocate space for local variable
 
-	fld dword [ebp+8]      ; load float value from argument [ebp+8]
+	fld qword [ebp+8]      ; load double value from argument [ebp+8]
 	fld st0                ; clone top of the stack
 	fmul st1               ; multiply st0 and st1, write to st0
 	fmulp st1, st0         ; multiply st0 and st1, pop
