a0e64cecab7a77bc35177e89acf30f474f8b39cf
mon1t0r <westxlebyshek@gmail.com>
2025-06-14 13:15:57 +0200

    sscan_lx complex implementation

--- libs/libutils/src/string.asm
+++ libs/libutils/src/string.asm
@@ -1,4 +1,5 @@
 global str_len
+global strncpy
 
 ; str_len (char_ptr)
 str_len:
@@ -18,5 +19,24 @@ str_len:
 	sub eax, edx      ; subtract start pointer from end pointer
 
 	mov esp, ebp      ; epilogue
+	pop ebp
+	ret
+
+; strncpy (char_ptr_dst, char_ptr_src, int_cnt)
+strncpy:
+	push ebp          ; prologue
+	mov ebp, esp
+	push edi
+	push esi
+
+	mov edi, [ebp+8]  ; move destination pointer to EDI
+	mov esi, [ebp+12] ; move source pointer to ESI
+	mov ecx, [ebp+16] ; move char count to ECX
+
+	rep movsb
+
+	pop esi           ; epilogue
+	pop edi
+	mov esp, ebp
 	pop ebp
 	ret
